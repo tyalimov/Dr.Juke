cca6fa9d11bd5d5ec6e4fb81d724b1dab306a61b
Pavel Knyazev <pasha2.05.97@mail.ru>
2020-01-05 03:22:59 +0300

    Tmp: registry notify work condition

--- AVKernel/main.cpp
+++ AVKernel/main.cpp
@@ -52,17 +52,72 @@ using namespace eastl;
 //   BOOLEAN          Asynchronous
 // );
 
-BOOLEAN ReadPreferences(LPCWSTR reg_path)
+
+struct CNotificationEvent
 {
-	BOOLEAN ok;
+	HANDLE hEvent = nullptr;
+	PKEVENT pkEvent = nullptr;
+
+	CNotificationEvent(LPCWSTR name)
+	{
+		UNICODE_STRING us;
+		RtlInitUnicodeString(&us, name);
+		pkEvent = IoCreateNotificationEvent(&us, &hEvent);
+		kprintf(TRACE_INFO, "EventName=%ws, pkEvent=0x%p, hEvent=0x%p", name, pkEvent, hEvent);
+		KeClearEvent(pkEvent);
+		LONG state = KeResetEvent(pkEvent);
+		kprintf(TRACE_INFO, "state=%d", state);
+	}
+
+	~CNotificationEvent() {
+		ZwClose(hEvent);
+	}
+};
+
+unique_ptr<CNotificationEvent> g_nf;
+
+NTSTATUS SetupKeyValueChangeNotifier(LPCWSTR reg_path, HANDLE hEvent, PIO_STATUS_BLOCK pio)
+{
+	NTSTATUS status;
+	OBJECT_ATTRIBUTES oa;
+	UNICODE_STRING KeyPath;
+	HANDLE hKey;
+	
+	UNREFERENCED_PARAMETER(reg_path);
+	UNREFERENCED_PARAMETER(hEvent);
+
+	RtlInitUnicodeString(&KeyPath, L"\\Registry\\Machine\\SOFTWARE\\RegisteredApplications");
+	InitializeObjectAttributes(&oa, &KeyPath, OBJ_CASE_INSENSITIVE, NULL, NULL);
+	status = ZwOpenKeyEx(&hKey, KEY_NOTIFY, &oa, 0);
+
+	if (NT_SUCCESS(status))
+	{
+		status = ZwNotifyChangeKey(hKey, g_nf->hEvent, NULL, (PVOID)DelayedWorkQueue, 
+			pio, REG_NOTIFY_CHANGE_LAST_SET, FALSE, NULL, 0, TRUE);
+
+		kprintf(TRACE_INFO, "status=0x%08X signaled state=%d", status, KeReadStateEvent(g_nf->pkEvent));
+		status = KeWaitForSingleObject(g_nf->pkEvent, Executive, KernelMode, FALSE, NULL);
+		kprintf(TRACE_INFO, "after wait status=0x%08X", status);
+		while (1);
+
+		ZwClose(hKey);
+	}
+
+	kprintf(TRACE_INFO, "Exit with status 0x%08X", status);
+	return status;
+}
+
+NTSTATUS ReadPreferences(LPCWSTR reg_path)
+{
 	NTSTATUS status;
 	HANDLE hKey;
 	OBJECT_ATTRIBUTES oa;
 	UNICODE_STRING KeyPath;
+	ULONG DataSize;
+	ULONG idx = 0;
 
 	UNREFERENCED_PARAMETER(reg_path);
 
-	ok = FALSE;
 	//HKEY_LOCAL_MACHINE\SOFTWARE\RegisteredApplications
 	RtlInitUnicodeString(&KeyPath, L"\\Registry\\Machine\\SOFTWARE\\RegisteredApplications");
 	InitializeObjectAttributes(&oa, &KeyPath, OBJ_CASE_INSENSITIVE, NULL, NULL);
@@ -70,49 +125,47 @@ BOOLEAN ReadPreferences(LPCWSTR reg_path)
 
 	if (NT_SUCCESS(status))
 	{
-		ULONG data_size;
-		ULONG idx = 0;
-
 		while (1)
 		{
-			data_size = 0;
-			status = ZwEnumerateValueKey(hKey, idx, KeyValueFullInformation, NULL, 0, &data_size);
+			DataSize = 0;
+			status = ZwEnumerateValueKey(hKey, idx, KeyValueFullInformation, NULL, 0, &DataSize);
 			if (status == STATUS_NO_MORE_ENTRIES)
-				break;
-			if (status == STATUS_INVALID_PARAMETER)
-				break;
-			
-			if (data_size > 0)
 			{
-				CHAR* kvfi_ch = new CHAR[data_size];  
+				status = STATUS_SUCCESS;
+				break;
+			}
+
+			if (!NT_SUCCESS(status))
+				break;
+
+			if (DataSize > 0)
+			{
+				CHAR* kvfi_ch = new CHAR[DataSize];
 				KEY_VALUE_FULL_INFORMATION* kvfi = (KEY_VALUE_FULL_INFORMATION*)kvfi_ch;
 				if (kvfi)
 				{
-					status = ZwEnumerateValueKey(hKey, idx, KeyValueFullInformation, kvfi, data_size, &data_size);
-					kprintf(TRACE_INFO, "%d) on enumerate status=0x%08X, type=%d", idx, RtlNtStatusToDosError(status), kvfi->Type);
+					status = ZwEnumerateValueKey(hKey, idx, KeyValueFullInformation, kvfi, DataSize, &DataSize);
 					if (NT_SUCCESS(status) && (kvfi->Type == REG_SZ))
 					{
 						PWCH buf;
 						ULONG len;
-						
+
 						buf = (PWCH)(kvfi_ch + kvfi->DataOffset);
 						len = kvfi->DataLength;
-					 	wstring data_w(buf, len);
-						kprintf(TRACE_INFO, "%ws", data_w.c_str());
+						wstring data_w(buf, len);
+						kprintf(TRACE_PREF, "%ws", data_w.c_str());
 
-					// 	buf = kvfi->Name;
-					// 	len = kvfi->NameLength;
-					// 	wstring name_w(buf, len);
+						// 	buf = kvfi->Name;
+						// 	len = kvfi->NameLength;
+						// 	wstring name_w(buf, len);
 
-					// 	kprintf(TRACE_INFO, "%s) Name=%ws, Data=%ws", idx, data_w.c_str(), name_w.c_str());
+						// 	kprintf(TRACE_INFO, "%s) Name=%ws, Data=%ws", idx, data_w.c_str(), name_w.c_str());
 					}
 
 					delete[] kvfi_ch;
 				}
 				else
-				{
-					kprintf(TRACE_INFO, "Empty record");
-				}
+					kprintf(TRACE_PREF, "Empty record");
 
 				idx++;
 			}
@@ -121,40 +174,50 @@ BOOLEAN ReadPreferences(LPCWSTR reg_path)
 		ZwClose(hKey);
 	}
 
-	kprintf(TRACE_INFO, "exit with status 0x%08X", RtlNtStatusToDosError(status));
-	return ok;
+	kprintf(TRACE_PREF, "Read %d entries. Exit with status 0x%08X", idx, status);
+	return status;
 }
+
 
 typedef struct _THREAD_CTX {
 	KEVENT evKill;
 	PKTHREAD thread;
  } *PTHREAD_CTX, THREAD_CTX;
 
-KEVENT dummy;
-
 VOID ThreadProc(PTHREAD_CTX ctx)
 {
 	NTSTATUS status;
-	PVOID objects[] = { &ctx->evKill, &dummy };
 
-	kprintf(TRACE_INFO, "Registry listener is running");
+	kprintf(TRACE_THREAD, "Registry listener is running");
 
-	ReadPreferences(L"aaa");
+	//ReadPreferences(L"aaa");
+	IO_STATUS_BLOCK iob = { 0 };
+	status = SetupKeyValueChangeNotifier(L"aaa", g_nf->hEvent, &iob);
+	if (status != STATUS_PENDING)
+		goto exit;
 
+	PVOID objects[] = { &ctx->evKill, g_nf->pkEvent };
+	int i = 0;
 	while (TRUE)
 	{
-		// KeWaitForSingleObject(&ctx->evKill, Executive, KernelMode, FALSE, NULL);
 		status = KeWaitForMultipleObjects(RTL_NUMBER_OF(objects), 
 			objects, WaitAny, Executive, KernelMode, FALSE, NULL, NULL);
-
+	
 		if (!NT_SUCCESS(status) || status == STATUS_WAIT_0)
 			break;
 
+		if (i++ < 100)
+		{
+			kprintf(TRACE_INFO, "Registry change: cnt=%d, status=0x%08X", (ULONG)iob.Information, iob.Status);
+			status = SetupKeyValueChangeNotifier(L"aaa", g_nf->hEvent, &iob);
+			kprintf(TRACE_INFO, "status=0x%08X", status);
+		}
 		// WAIT_STATUS_1
 		// NTSYSAPI NTSTATUS ZwNotifyChangeKey(
 	}
 
-	kprintf(TRACE_INFO, "Registry listener exited with status 0x%08X", status);
+exit:
+	kprintf(TRACE_THREAD, "Registry listener exited with status 0x%08X", status);
 	PsTerminateSystemThread(status);
 }
 
@@ -163,43 +226,42 @@ NTSTATUS StartThread(PTHREAD_CTX ctx)
 	NTSTATUS status;
 	HANDLE hThread;
 
-	kprintf(TRACE_INFO, "Starting registry listener...");
+	kprintf(TRACE_THREAD, "Starting registry listener...");
 
 	// Initialize event for stopping thread
 	KeInitializeEvent(&ctx->evKill, NotificationEvent, FALSE);
-	KeInitializeEvent(&dummy, NotificationEvent, FALSE);
 
 	// Create thread
 	status = PsCreateSystemThread(&hThread, THREAD_ALL_ACCESS,
 	  NULL, NULL, NULL, (PKSTART_ROUTINE) ThreadProc, ctx);
 	
 	if (!NT_SUCCESS(status))
-	  return status;
+		return status;
 	
 	// Increment object counter
 	ObReferenceObjectByHandle(hThread, THREAD_ALL_ACCESS, NULL,
 	  KernelMode, (PVOID*) &ctx->thread, NULL);
 
 	ZwClose(hThread);
-	kprintf(TRACE_INFO, "Starting registry listener... ok");
+	kprintf(TRACE_THREAD, "Starting registry listener... ok");
 	return STATUS_SUCCESS;
 }
 
 VOID StopThread(PTHREAD_CTX ctx)
 {
-	kprintf(TRACE_INFO, "Stopping registry listener...");
+	kprintf(TRACE_THREAD, "Stopping registry listener...");
 	
 	KeSetEvent(&ctx->evKill, 0, FALSE);
 	KeWaitForSingleObject(ctx->thread, Executive, KernelMode, FALSE, NULL);
 	
 	ObDereferenceObject(ctx->thread);
-	kprintf(TRACE_INFO, "Stopping registry listener... ok");
+	kprintf(TRACE_THREAD, "Stopping registry listener... ok");
 }
 
 
 
 
-unique_ptr<THREAD_CTX> g_ctx = nullptr;
+unique_ptr<THREAD_CTX> g_ctx;
 
 
 NTSTATUS SysMain(PDRIVER_OBJECT DrvObject, PUNICODE_STRING RegPath) {
@@ -212,6 +274,8 @@ NTSTATUS SysMain(PDRIVER_OBJECT DrvObject, PUNICODE_STRING RegPath) {
 	};	
 
 	g_ctx = make_unique<THREAD_CTX>();
+	g_nf = make_unique<CNotificationEvent>( L"\\BaseNamedObjects\\TestEvent");
+	
 
 	StartThread(g_ctx.get());
 	kprintf(TRACE_INFO, "Driver is loaded");
