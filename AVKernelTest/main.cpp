ec60e99f1841dd7c9a7f9015ff3e523fe779ed2f
Pavel Knyazev <pasha2.05.97@mail.ru>
2020-01-18 22:03:50 +0300

    Added API to write driver configuration to registry keys

--- AVKernelTest/main.cpp
+++ AVKernelTest/main.cpp
@@ -1,9 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <vld.h>
+//#include <vld.h>
 #include <windows.h>
 #include <string>
+#include <stdio.h>
 
 using namespace std;
+
+#define KPM_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PsMonitor\\ProtectedObjects" 
+#define KRF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\RegFilter\\ProtectedObjects"
+#define KRF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\RegFilter\\AllowedProcesses" 
+#define KFF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\ProtectedObjects"
+#define KFF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\AllowedProcesses" 
+#define KPF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PipeFilter\\ProtectedObjects"
+#define KPF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PipeFilter\\AllowedProcesses" 
 
 typedef LONG NTSTATUS;
 
@@ -55,6 +64,22 @@ DWORD WriteRegistryKey(HKEY hBaseKey, LPCWSTR lpSubKey,
 		Ret = RegSetValueEx(hKey, ValueName, 0, 
 			DataType, (BYTE*)Data, DataLen);
 
+		RegCloseKey(hKey);
+	}
+
+	return Ret;
+}
+
+DWORD DeleteRegistryValue(HKEY hBaseKey, LPCWSTR lpSubKey, LPCWSTR ValueName)
+{
+	DWORD Ret;
+	HKEY hKey;
+
+	Ret = RegOpenKeyEx(hBaseKey, lpSubKey, 0, KEY_WRITE, &hKey);
+
+	if (Ret == ERROR_SUCCESS && hKey != NULL)
+	{	
+        Ret = RegDeleteValue(hKey, ValueName);
 		RegCloseKey(hKey);
 	}
 
@@ -146,17 +171,6 @@ wstring GetPipeKernelPath(const wstring& pipe_name)
     return wstring();
 }
 
-#define KPM_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PsMonitor\\ProtectedObjects" 
-
-#define KRF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\RegFilter\\ProtectedObjects"
-#define KRF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\RegFilter\\AllowedProcesses" 
-
-#define KFF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\ProtectedObjects"
-#define KFF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\AllowedProcesses" 
-
-#define KPF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PipeFilter\\ProtectedObjects"
-#define KPF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PipeFilter\\AllowedProcesses" 
-
 enum FilterDriverType
 {
 	FsFilter,
@@ -164,7 +178,7 @@ enum FilterDriverType
 	RegFilter
 };
 
-bool AddAllowedProcess(wstring image_path, FilterDriverType type)
+bool AllowAccessToObject(wstring ps_image_path, FilterDriverType type, bool cancel_allow=false)
 {
     const wchar_t* lpSubkey;
     DWORD res;
@@ -184,34 +198,25 @@ bool AddAllowedProcess(wstring image_path, FilterDriverType type)
 		return false;
 	}
 
-    image_path = GetFileKernelPath(image_path);
-    if (image_path.length() == 0)
+    ps_image_path = GetFileKernelPath(ps_image_path);
+    if (ps_image_path.length() == 0)
         return false;
 
-    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, 
-        lpSubkey, image_path.c_str(), NULL, 0, REG_SZ);
+    if (cancel_allow)
+    {
+        res = DeleteRegistryValue(HKEY_LOCAL_MACHINE,
+            lpSubkey, ps_image_path.c_str());
+    }
+    else
+    {
+        res = WriteRegistryKey(HKEY_LOCAL_MACHINE,
+            lpSubkey, ps_image_path.c_str(), NULL, 0, REG_SZ);
+    }
 
 	return res == ERROR_SUCCESS;
 }
 
-bool AddProtectedProcess(wstring image_path)
-{
-    const wchar_t* lpSubkey;
-    DWORD res;
-
-    lpSubkey = KPM_PROTECTED_OBJECTS;
-    image_path = GetFileKernelPath(image_path);
-    
-    if (image_path.length() == 0)
-        return false;
-
-    res = WriteRegistryKey(HKEY_LOCAL_MACHINE,
-        lpSubkey, image_path.c_str(), NULL, 0, REG_SZ);
-
-	return res == ERROR_SUCCESS;
-}
-
-bool AddProtectedFile(wstring file_path, ACCESS_MASK access)
+bool ProtectFile(wstring file_path, ACCESS_MASK access=0, bool deprotect=false)
 {
     const wchar_t* lpSubkey;
     DWORD res;
@@ -222,13 +227,21 @@ bool AddProtectedFile(wstring file_path, ACCESS_MASK access)
     if (file_path.length() == 0)
         return false;
 
-    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
-        file_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+    if (deprotect)
+    {
+        res = DeleteRegistryValue(HKEY_LOCAL_MACHINE,
+            lpSubkey, file_path.c_str());
+    }
+    else
+    {
+		res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
+			file_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+    }
 
 	return res == ERROR_SUCCESS;
 }
 
-bool AddProtectedPipe(wstring pipe_path, ACCESS_MASK access)
+bool ProtectPipe(wstring pipe_path, ACCESS_MASK access=0, bool deprotect=false)
 {
     const wchar_t* lpSubkey;
     DWORD res;
@@ -239,13 +252,21 @@ bool AddProtectedPipe(wstring pipe_path, ACCESS_MASK access)
     if (pipe_path.length() == 0)
         return false;
 
-    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
-        pipe_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+    if (deprotect)
+    {
+        res = DeleteRegistryValue(HKEY_LOCAL_MACHINE,
+            lpSubkey, pipe_path.c_str());
+    }
+    else
+    {
+		res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
+			pipe_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+    }
 
 	return res == ERROR_SUCCESS;
 }
 
-bool AddProtectedKey(HKEY base, wstring subkey_path, ACCESS_MASK access)
+bool ProtectKey(HKEY base, wstring subkey_path, ACCESS_MASK access=0, bool deprotect=false)
 {
     const wchar_t* lpSubkey;
     DWORD res;
@@ -256,27 +277,228 @@ bool AddProtectedKey(HKEY base, wstring subkey_path, ACCESS_MASK access)
     if (subkey_path.length() == 0)
         return false;
 
-    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
-        subkey_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+	if (deprotect)
+	{
+        res = DeleteRegistryValue(HKEY_LOCAL_MACHINE,
+            lpSubkey, subkey_path.c_str());
+	}
+	else
+	{
+		res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
+			subkey_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+	}
+
 
 	return res == ERROR_SUCCESS;
 }
 
-int main()
+bool ProtectProcess(wstring ps_image_path, bool deprotect=false)
 {
-	// GetKeyKernelPath(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft");
-	// GetFileKernelPath(L"main.cpp");
-	// GetPipeKernelPath(L"Dropboxpipe1");
-    const wchar_t* p = L"c:\\Program Files (x86)\\Adobe\\Acrobat Reader DC\\Reader\\AcroBroker.exe";
-    const wchar_t* f = L"c:\\Program Files (x86)\\Adobe\\Acrobat Reader DC\\Reader\\A3DUtils.dll";
-    const wchar_t* k = L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\ProtectedObjects";
-    AddAllowedProcess(p, FsFilter);
-    AddAllowedProcess(p, PipeFilter);
-    AddAllowedProcess(p, RegFilter);
-    AddProtectedProcess(p);
-    AddProtectedFile(f, 0x12345);
-    AddProtectedKey(HKEY_LOCAL_MACHINE, k, 0x123456);
-    AddProtectedPipe(L"DropboxPipe_1", 0x1234567);
+    const wchar_t* lpSubkey;
+    DWORD res;
 
-	return 0;
+    lpSubkey = KPM_PROTECTED_OBJECTS;
+    ps_image_path = GetFileKernelPath(ps_image_path);
+    
+    if (ps_image_path.length() == 0)
+        return false;
+
+    if (deprotect)
+    {
+        res = DeleteRegistryValue(HKEY_LOCAL_MACHINE,
+            lpSubkey, ps_image_path.c_str());
+    }
+    else
+    {
+		res = WriteRegistryKey(HKEY_LOCAL_MACHINE,
+			lpSubkey, ps_image_path.c_str(), NULL, 0, REG_SZ);
+    }
+
+	return res == ERROR_SUCCESS;
+}
+
+bool TestOpenKey(HKEY base, const wstring& subkey_path, ACCESS_MASK access)
+{
+    HKEY hKey;
+    DWORD Ret;
+    
+    Ret = RegOpenKeyEx(base, subkey_path.c_str(), 0, access, &hKey);
+	if (Ret == ERROR_SUCCESS && hKey != NULL)
+	{	
+		RegCloseKey(hKey);
+        return true;
+	}
+
+    return false;
+}
+
+bool TestOpenFile(const wstring& file_path, ACCESS_MASK access)
+{
+	HANDLE hPipe = CreateFile( 
+			 file_path.c_str(),   // pipe name 
+			 access,    // read and write access 
+			 0,              // no sharing 
+			 NULL,           // default security attributes
+			 OPEN_EXISTING,  // opens existing pipe 
+			 0,              // default attributes 
+			 NULL);          // no template file 
+
+    if (hPipe != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(hPipe);
+        return true;
+    }
+
+    return false;
+}
+
+bool TestOpenPipe(wstring pipe_name, ACCESS_MASK access)
+{
+    pipe_name = L"\\\\.\\pipe\\" + pipe_name;
+    return TestOpenFile(pipe_name, access);
+}
+
+bool TestOpenProcess(DWORD pid, ACCESS_MASK access)
+{
+    HANDLE hProcess = OpenProcess(access, FALSE, pid);
+    if (hProcess != NULL)
+    {
+        CloseHandle(hProcess);
+        return true;
+    }
+
+    return false;
+}
+
+void usage()
+{
+    // protect key SOFTWARE\Microsoft 0x00020000
+    // protect pipe DropboxPipe_1 0x00020000
+    // protect file c:\test.exe 0x00020000
+    // protect ps c:\test.exe 0x00020000
+
+    // deprotect key SOFTWARE\Microsoft
+    // deprotect pipe DropboxPipe_1
+    // deprotect file c:\test.exe
+    // deprotect ps c:\test.exe
+
+    wprintf(L"Usage:\n"); 
+    wprintf(L"   protect key <key-path> <access-mask-hex>\n");
+    wprintf(L"   protect pipe <pipe-name> <access-mask-hex>\n");
+    wprintf(L"   protect file <file-path> <access-mask-hex>\n");
+    wprintf(L"   protect ps <process-path> <access-mask-hex>\n");
+    wprintf(L"\n");
+    wprintf(L"   deprotect key <key-path>\n");
+    wprintf(L"   deprotect pipe <pipe-name>\n");
+    wprintf(L"   deprotect file <file-path>\n");
+    wprintf(L"   deprotect ps <process-path>\n");
+    wprintf(L"\n");
+    wprintf(L"   test-open key <key-path> <access-mask-hex>\n");
+    wprintf(L"   test-open pipe <pipe-name> <access-mask-hex>\n");
+    wprintf(L"   test-open file <file-path> <access-mask-hex>\n");
+    wprintf(L"   test-open ps <pid> <access-mask-hex>\n");
+    wprintf(L"\n");
+    wprintf(L"   allow key <key-path> <ps-image-path>\n");
+    wprintf(L"   allow pipe <pipe-name> <ps-image-path>\n");
+    wprintf(L"   allow file <file-path> <ps-image-path>\n");
+    wprintf(L"\n");
+    wprintf(L"   deny key <key-path> <ps-image-path>\n");
+    wprintf(L"   deny pipe <pipe-name> <ps-image-path>\n");
+    wprintf(L"   deny file <file-path> <ps-image-path>\n");
+}
+
+int wmain(int argc, wchar_t *argv[])
+{
+    if (argc == 1)
+    {
+        usage();
+        return 0;
+    }
+
+    if (argc == 5)
+    {
+		DWORD access = (DWORD)wcstol(argv[4], NULL, 16);
+		LPCWSTR path = argv[3];
+		bool ok = false;
+
+        if (!wcscmp(argv[1], L"protect"))
+        {
+			if (!wcscmp(argv[2], L"key"))
+				ok = ProtectKey(HKEY_LOCAL_MACHINE, path, access);
+			else if (!wcscmp(argv[2], L"file"))
+				ok = ProtectFile(path, access);
+			else if (!wcscmp(argv[2], L"pipe"))
+				ok = ProtectPipe(path, access);
+			else if (!wcscmp(argv[2], L"ps"))
+				ok = ProtectProcess(path);
+			else
+				wprintf(L"bad args\n");
+        }
+        else if (!wcscmp(argv[1], L"test-open"))
+        {
+            if (!wcscmp(argv[2], L"key"))
+                ok = TestOpenKey(HKEY_LOCAL_MACHINE, path, access);
+            else if (!wcscmp(argv[2], L"file"))
+                ok = TestOpenFile(path, access);
+            else if (!wcscmp(argv[2], L"pipe"))
+                ok = TestOpenPipe(path, access);
+            else if (!wcscmp(argv[2], L"ps"))
+                ok = TestOpenProcess(_wtol(argv[3]), access);
+			else
+				wprintf(L"bad args\n");
+        }
+        else if (!wcscmp(argv[1], L"allow"))
+        {
+            if (!wcscmp(argv[2], L"key"))
+                ok = AllowAccessToObject(path, RegFilter);
+			else if (!wcscmp(argv[2], L"file"))
+				ok = AllowAccessToObject(path, FsFilter);
+			else if (!wcscmp(argv[2], L"pipe"))
+				ok = AllowAccessToObject(path, PipeFilter);
+			else
+				wprintf(L"bad args\n");
+        }
+        else if (!wcscmp(argv[1], L"deny"))
+        {
+            if (!wcscmp(argv[2], L"key"))
+                ok = AllowAccessToObject(path, RegFilter, true);
+			else if (!wcscmp(argv[2], L"file"))
+				ok = AllowAccessToObject(path, FsFilter, true);
+			else if (!wcscmp(argv[2], L"pipe"))
+				ok = AllowAccessToObject(path, PipeFilter, true);
+			else
+				wprintf(L"bad args\n");
+        }
+        else
+			wprintf(L"bad args\n");
+
+        wprintf(L"\nResult: %s\n", ok ? L"Ok" : L"Failed");
+    }
+    else if (argc == 4)
+    {
+		LPCWSTR path = argv[3];
+		bool ok = false;
+
+        if (!wcscmp(argv[1], L"deprotect"))
+        {
+			if (!wcscmp(argv[2], L"key"))
+				ok = ProtectKey(HKEY_LOCAL_MACHINE, path, 0, true);
+			else if (!wcscmp(argv[2], L"file"))
+				ok = ProtectFile(path, 0, true);
+			else if (!wcscmp(argv[2], L"pipe"))
+				ok = ProtectPipe(path, 0, true);
+			else if (!wcscmp(argv[2], L"ps"))
+				ok = ProtectProcess(path, true);
+            else
+                wprintf(L"bad args\n");
+        }
+        else
+			wprintf(L"bad args\n");
+
+        wprintf(L"\nResult: %s\n", ok ? L"Ok" : L"Failed");
+    }
+    else
+        wprintf(L"bad args\n");
+
+    wprintf(L"\n");
 }
