8b1e3f7cf5b8d8f5f99c3bab619d5349dbd28959
Pavel Knyazev <pasha2.05.97@mail.ru>
2020-01-18 19:30:19 +0300

    Added API to control driver

--- AVKernelTest/main.cpp
+++ AVKernelTest/main.cpp
@@ -41,74 +41,24 @@ typedef DWORD (__stdcall *NtQueryKeyFunc)(
 	ULONG  Length,
 	PULONG  ResultLength);
 
-BOOL CreateRegistryKey(HKEY hKeyParent, PWCHAR subkey)
+
+DWORD WriteRegistryKey(HKEY hBaseKey, LPCWSTR lpSubKey, 
+	LPCWSTR ValueName, LPCVOID Data, DWORD DataLen, DWORD DataType)
 {
-	DWORD dwDisposition; //It verify new key is created or open existing key
-	HKEY  hKey;
 	DWORD Ret;
+	HKEY hKey;
 
-	Ret = RegCreateKeyEx(
-		hKeyParent,
-		subkey,
-		0,
-		NULL,
-		REG_OPTION_NON_VOLATILE,
-		KEY_ALL_ACCESS,
-		NULL,
-		&hKey,
-		&dwDisposition);
+	Ret = RegOpenKeyEx(hBaseKey, lpSubKey, 0, KEY_WRITE, &hKey);
 
-	if (Ret != ERROR_SUCCESS)
-	{
-		if (Ret == ERROR_ACCESS_DENIED)
-			printf("Error opening or creating new key: Access denied\n");
-		else
-			printf("Error opening or creating new key: Unknown error 0x%08X\n", Ret);
-
-		return FALSE;
-	}
-
-	RegCloseKey(hKey);
-	printf("Succesfully opened or created new key\n");
-	return TRUE;
-}
-
-BOOL WriteRegistryKey(HKEY hKeyParent, PWCHAR subkey, PWCHAR valueName, DWORD data)
-{
-	DWORD Ret; //use to check status
-	HKEY hKey; //key
-
-	Ret = RegOpenKeyEx(
-		hKeyParent,
-		subkey,
-		0,
-		KEY_WRITE,
-		&hKey
-		);
-
-	if (Ret == ERROR_SUCCESS)
+	if (Ret == ERROR_SUCCESS && hKey != NULL)
 	{	
-		DWORD status = RegSetValueEx(hKey, valueName, 0, 
-			REG_DWORD, reinterpret_cast<BYTE*>(&data), sizeof(data));
+		Ret = RegSetValueEx(hKey, ValueName, 0, 
+			DataType, (BYTE*)Data, DataLen);
 
-		if (status != ERROR_SUCCESS)
-		{
-			printf("Error writing key: 0x%08X\n", status);
-			RegCloseKey(hKey);
-			return FALSE;
-		}
-
-		//close the key
 		RegCloseKey(hKey);
-		printf("Successfull write to key\n");
-		return TRUE;
 	}
-	else if (Ret == ERROR_ACCESS_DENIED)
-		printf("Error opening or creating new key: Access denied\n");
-	else
-		printf("Error opening or creating new key: Unknown error 0x%08X\n", Ret);
 
-	return FALSE;
+	return Ret;
 }
 
 wstring GetFileKernelPath(const wstring& file_path)
@@ -176,29 +126,157 @@ wstring GetKeyKernelPath(HKEY hBaseKey, const wstring& lpSubKey)
 
 wstring GetPipeKernelPath(const wstring& pipe_name)
 {
-	return L"\\Device\\NamedPipe\\" + pipe_name;
+	wstring pipe_path = L"\\\\.\\pipe\\" + pipe_name;
+
+	HANDLE hPipe = CreateFile( 
+			 pipe_path.c_str(),   // pipe name 
+			 GENERIC_READ,    // read and write access 
+			 0,              // no sharing 
+			 NULL,           // default security attributes
+			 OPEN_EXISTING,  // opens existing pipe 
+			 0,              // default attributes 
+			 NULL);          // no template file 
+
+    if (hPipe != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(hPipe);
+        return L"\\Device\\NamedPipe\\" + pipe_name;
+    }
+
+    return wstring();
 }
 
-bool AddProtectedProcess(const wstring& image_path, ACCESS_MASK access)
-{
+#define KPM_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PsMonitor\\ProtectedObjects" 
 
+#define KRF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\RegFilter\\ProtectedObjects"
+#define KRF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\RegFilter\\AllowedProcesses" 
+
+#define KFF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\ProtectedObjects"
+#define KFF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\AllowedProcesses" 
+
+#define KPF_PROTECTED_OBJECTS L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PipeFilter\\ProtectedObjects"
+#define KPF_ALLOWED_PROCESSES L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\PipeFilter\\AllowedProcesses" 
+
+enum FilterDriverType
+{
+	FsFilter,
+	PipeFilter,
+	RegFilter
+};
+
+bool AddAllowedProcess(wstring image_path, FilterDriverType type)
+{
+    const wchar_t* lpSubkey;
+    DWORD res;
+
+	switch (type)
+	{
+    case FsFilter:
+		lpSubkey = KFF_ALLOWED_PROCESSES;
+		break;
+	case RegFilter:
+		lpSubkey = KRF_ALLOWED_PROCESSES;
+		break;
+	case PipeFilter:
+		lpSubkey = KPF_ALLOWED_PROCESSES;
+		break;
+	default:
+		return false;
+	}
+
+    image_path = GetFileKernelPath(image_path);
+    if (image_path.length() == 0)
+        return false;
+
+    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, 
+        lpSubkey, image_path.c_str(), NULL, 0, REG_SZ);
+
+	return res == ERROR_SUCCESS;
 }
 
-bool AddProtectedFile(const wstring& image_path, ACCESS_MASK access)
+bool AddProtectedProcess(wstring image_path)
 {
+    const wchar_t* lpSubkey;
+    DWORD res;
 
+    lpSubkey = KPM_PROTECTED_OBJECTS;
+    image_path = GetFileKernelPath(image_path);
+    
+    if (image_path.length() == 0)
+        return false;
+
+    res = WriteRegistryKey(HKEY_LOCAL_MACHINE,
+        lpSubkey, image_path.c_str(), NULL, 0, REG_SZ);
+
+	return res == ERROR_SUCCESS;
 }
 
-bool AddProtectedPipe(const wstring& image_path, ACCESS_MASK access)
+bool AddProtectedFile(wstring file_path, ACCESS_MASK access)
 {
+    const wchar_t* lpSubkey;
+    DWORD res;
 
+    lpSubkey = KFF_PROTECTED_OBJECTS;
+    file_path = GetFileKernelPath(file_path);
+
+    if (file_path.length() == 0)
+        return false;
+
+    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
+        file_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+
+	return res == ERROR_SUCCESS;
+}
+
+bool AddProtectedPipe(wstring pipe_path, ACCESS_MASK access)
+{
+    const wchar_t* lpSubkey;
+    DWORD res;
+
+    lpSubkey = KPF_PROTECTED_OBJECTS;
+    pipe_path = GetPipeKernelPath(pipe_path);
+
+    if (pipe_path.length() == 0)
+        return false;
+
+    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
+        pipe_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+
+	return res == ERROR_SUCCESS;
+}
+
+bool AddProtectedKey(HKEY base, wstring subkey_path, ACCESS_MASK access)
+{
+    const wchar_t* lpSubkey;
+    DWORD res;
+
+    lpSubkey = KRF_PROTECTED_OBJECTS;
+    subkey_path = GetKeyKernelPath(base, subkey_path);
+
+    if (subkey_path.length() == 0)
+        return false;
+
+    res = WriteRegistryKey(HKEY_LOCAL_MACHINE, lpSubkey, 
+        subkey_path.c_str(), &access, sizeof(ACCESS_MASK), REG_DWORD);
+
+	return res == ERROR_SUCCESS;
 }
 
 int main()
 {
-	GetKeyKernelPath(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft");
-	GetFileKernelPath(L"main.cpp");
-	GetPipeKernelPath(L"Dropboxpipe1");
+	// GetKeyKernelPath(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft");
+	// GetFileKernelPath(L"main.cpp");
+	// GetPipeKernelPath(L"Dropboxpipe1");
+    const wchar_t* p = L"c:\\Program Files (x86)\\Adobe\\Acrobat Reader DC\\Reader\\AcroBroker.exe";
+    const wchar_t* f = L"c:\\Program Files (x86)\\Adobe\\Acrobat Reader DC\\Reader\\A3DUtils.dll";
+    const wchar_t* k = L"SOFTWARE\\Dr.Juke\\AVSecGeneric\\FsFilter\\ProtectedObjects";
+    AddAllowedProcess(p, FsFilter);
+    AddAllowedProcess(p, PipeFilter);
+    AddAllowedProcess(p, RegFilter);
+    AddProtectedProcess(p);
+    AddProtectedFile(f, 0x12345);
+    AddProtectedKey(HKEY_LOCAL_MACHINE, k, 0x123456);
+    AddProtectedPipe(L"DropboxPipe_1", 0x1234567);
 
 	return 0;
 }
