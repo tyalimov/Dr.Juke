d5e9714354d0b9261e786a4ba60dad580e86cfd7
tyalimov <lazmible@mail.ru>
2020-02-13 02:30:59 +0300

    GUI

---
+++ gui/test.py
@@ -0,0 +1,13 @@
+request = """
+{{
+    "task" : "add_protected_object",
+    "parameters"  : 
+    {{
+        "path"    : "{0}",
+        "type"    :  0,
+        "content" :  {1}
+    }}
+}}""".format("dsada", "dsadas")
+
+
+print(request)
